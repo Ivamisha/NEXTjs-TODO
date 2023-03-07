ca08c294a563b8c0f06dcf9bc46b88a61116acc5
Mikhail <m.ivanuschenko@gmail.com>
2023-03-07 23:51:01 +0300

    fix

--- HOC/modals/CreateTodoModal.tsx
+++ HOC/modals/CreateTodoModal.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import  { FC } from 'react'
 import styles from '../../styles/Modal.module.scss'
 import { useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
